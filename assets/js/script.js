64574bc968bfe56892ff94c79c044d5e6d95e671
Jacob Liberty <jacob.tobin.liberty@gmail.com>
2021-12-30 17:37:22 -0500

    buttons load properly and fixed error handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ const SaveCityKey = 'City';
 function weather(cityName) {
   // Make sure there is a city name
   if (cityName === ''){
-    console.log('error')
+    console.log('No city inputted')
     return
   }
   const APIkey = '5a84980c7b7b783f90dce101ba33a6f6';
@@ -14,12 +14,17 @@ function weather(cityName) {
   // Convert data to json
   .then(function(resp) { return resp.json() })
   .then(function(data) {
-    loadForecast(data);
-    saveCity(cityName);
+    if (data.cod == '404') {
+      let cityName = document.getElementById('city');
+      cityName.value = 'Invalid City'
+    }else {
+      loadForecast(data);
+      saveCity(cityName);
+    }
   })
   // Catch errors
   .catch(function() {
-    console.log('Invalid City')
+    console.log('ERROR')
   });
 }
 
@@ -48,37 +53,52 @@ function loadForecast(data) {
   }
 }
 
-// Change/Update the date at the top of the page
-function changeDate() {
-  let dateText = document.getElementById('date');
-  dateText.innerHTML = date;
-}
-
 // Save the city name of inputted city to local storage
 function saveCity(cityName) {
-  let numberOfCities = localStorage.length;
-  localStorage.setItem(SaveCityKey + cityName, cityName);
-  // Check if a new city was added, if user inputs same city, don't create a new button
-  if (numberOfCities != localStorage.length) {
-    // Create new button for new saved city
-    $('#buttons').append(`<button class='btn btn-primary btn-m' id='${cityName}'>${cityName}</button>`);
-    newListener(cityName);
-  }  
+  // Check if city has already been saved
+  let checkCityStorage = true;
+  for (let i=0; i<localStorage.length; i++){
+    if (localStorage.getItem(SaveCityKey + i) == cityName){
+      checkCityStorage = false;
+    }
+  }
+  if (checkCityStorage == true) {
+    // save to local storage and create new button for new saved city
+    localStorage.setItem(SaveCityKey + localStorage.length, cityName);
+    $('#buttons').append(`<button class='btn btn-primary btn-m' id='${SaveCityKey + localStorage.length}'>${cityName}</button>`);
+    newListener(SaveCityKey + localStorage.length);
+  }
+}
+
+// Function to add local storage data and current date to page on load
+function loadPage() {
+  // Change page date
+  let dateText = document.getElementById('date');
+  dateText.innerHTML = date;
+  // loop through all saved cities in local storage
+  for (let i=0; i<localStorage.length; i++){
+    // get city and add it as a button
+    let savedCityName = localStorage.getItem(SaveCityKey + i);
+    $('#buttons').append(`<button class='btn btn-primary btn-m' id='${SaveCityKey + i}'>${savedCityName}</button>`);
+    // Add event listener to button
+    newListener(SaveCityKey + i);
+  }
 }
 
 // Event Listeners
-// Create Event Listener Function
-function newListener(cityName) {
-  let newButton = document.getElementById(`${cityName}`);
-  newButton.addEventListener('click', function() {weather(newButton.innerHTML)});
+// Create listener function
+function newListener(cityKey) {
+  document.getElementById(`${cityKey}`).addEventListener('click', function() {
+    let newCityName = document.getElementById(`${cityKey}`).innerHTML;
+    weather(newCityName);
+  });
 }
 // Add City button
 let newCity = document.getElementById('add-city');
 newCity.addEventListener('click', function(){
   let cityName = document.getElementById('city').value;
   weather(cityName);
-  console.log(cityName)
 });
 
 // Run on load
-changeDate();
+loadPage()
