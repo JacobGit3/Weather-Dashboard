c3d66f3a9515f84e4a6c261f5b091584abaa4391
Jacob Liberty <jacob.tobin.liberty@gmail.com>
2021-12-31 18:57:48 -0500

    final commit, added UV Index, Added Styling, ReadMe file edited

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # Weather-Dashboard
-This is a page that can show you the 7 day weather forecast for searched cities
+
+## Purpose
+
+Create a Webpage capable of finding weather data based on a city
+Save searched cities for later use as a button
+Show a 5 day forecast as well as current day
+Display Date, Icon, Temperature, Wind, Humidity, and UV Index for current day
+Ability to repopulate with new weather data when a different city is searched for or button clicked on.
+
+## Built With
+
+- HTML
+- CSS
+- Javascript
+- Jquery
+- Open Weather API
+- Bootstrap
+
+## Web Page
+The running application can be accessed here for viewing and testing:
+
+https://jacobgit3.github.io/Weather-Dashboard/
+
+The code for the application can be found in this github repository:
+
+https://github.com/JacobGit3/Weather-Dashboard
+
+This is a screenshot of the web page before a search is made
+
+![Screenshot](./assets/images/not_populated.PNG)
+
+This is a screenshot of the web page after it is populated with weather data
+
+![Screenshot](./assets/images/populated.PNG)
+
+## Contribution
+
+Code written by Jacob Liberty
+
+Inital Code from Trilogy Education
+
+Adapted some Icon and UV code from Ward Vanlaar 
+Thanks for the help Ward!
+Here is a link to his repository - https://github.com/WardVanlaar/weather-dashboard
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,66 @@
 // Global Variables
 let date = moment().format('L');
 const SaveCityKey = 'City';
+const APIkey = '5a84980c7b7b783f90dce101ba33a6f6';
+
 
 // Get the weather for the desired City
 function weather(cityName) {
   // Make sure there is a city name
   if (cityName === ''){
-    console.log('No city inputted')
-    return
+    console.log('No city inputted');
+    return;
   }
-  const APIkey = '5a84980c7b7b783f90dce101ba33a6f6';
   fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=metric&appid=' + APIkey)
   // Convert data to json
   .then(function(resp) { return resp.json() })
   .then(function(data) {
     if (data.cod == '404') {
+      cityName.value = 'Invalid City';
       let cityName = document.getElementById('city');
-      cityName.value = 'Invalid City'
     }else {
       loadForecast(data);
       saveCity(cityName);
+      UV_Title(data, cityName);
     }
   })
   // Catch errors
   .catch(function() {
-    console.log('ERROR')
+    console.log('ERROR');
   });
+}
+
+// Get and create UV index, also adds city name to title
+function UV_Title(data, cityName){
+  // Change city name in title
+  document.getElementById('yourCity').innerHTML = cityName;
+  // Get longitude and latitude from UV api
+  let latitude = data.city.coord.lat;
+  let longitude = data.city.coord.lon;
+  let UV_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,daily&appid=${APIkey}`;
+  return fetch(UV_URL)
+    .then(response => response.json())
+    .then(UVData => {
+      // Get UV index and run function to add UV index and colour it
+      let UVIndex = UVData.current.uvi;
+      UVColor(UVIndex);
+    })
+}
+
+// Add and change the background color of the UV div
+function UVColor(UVIndex){
+  // Check UV Index and change colour based on severity
+  let severity = '';
+  if (UVIndex < 2) {
+    severity = 'low';
+  }else if ((UVIndex >= 2) & (UVIndex < 5)) {
+    severity = 'medium';
+  }else {
+    severity = 'high';
+  }
+  let UVDiv = document.getElementById('UVIndex');
+  UVDiv.innerHTML = (`Current UV: ${UVIndex}`); 
+  UVDiv.classList.add(`${severity}`);
 }
 
 // Create the Daily Forecast divs 
@@ -33,14 +68,14 @@ function loadForecast(data) {
   // Create divs if none
   if ( $('#forecast').children().length == 0 ){
     for (let i=0; i<6; i++){
-      let day = moment().add(i, 'days').format('L')
+      let day = moment().add(i, 'days').format('L');
       let icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.list[i].weather[0].icon}.svg`;
       $('#forecast').append(`<div class='dailyForecast' id='forecast${i}'></div>`);
-      $(`#forecast${i}`).append(`<h3 class='forecastdate' id='date${i}'>${day}</h3>`);
+      $(`#forecast${i}`).append(`<h3 class='forecastDate' id='date${i}'>${day}</h3>`);
       $(`#forecast${i}`).append(`<image class='forecastIcon' id='icon${i}' src='${icon}'></image>`);
-      $(`#forecast${i}`).append(`<p class='forecastTemp' id='temp${i}'>Temp: ${data.list[i].main.temp}°C</p>`);
-      $(`#forecast${i}`).append(`<p class='forecastWind' id='wind${i}'>Wind: ${data.list[i].wind.speed} KPH</p>`);
-      $(`#forecast${i}`).append(`<p class='forecastHumid' id='humid${i}'>Humidity: ${data.list[i].main.humidity}%</p>`);
+      $(`#forecast${i}`).append(`<p class='forecastText' id='temp${i}'>Temp: ${data.list[i].main.temp}°C</p>`);
+      $(`#forecast${i}`).append(`<p class='forecastText' id='wind${i}'>Wind: ${data.list[i].wind.speed}kph</p>`);
+      $(`#forecast${i}`).append(`<p class='forecastText' id='humid${i}'>Humidity: ${data.list[i].main.humidity}%</p>`);
     };
   }
   // Rewrite content if there are divs 
@@ -105,4 +140,4 @@ newCity.addEventListener('click', function(){
 });
 
 // Run on load
-loadPage()
+loadPage();
